0a65a0cafabd9b1310bee51be8a4452f7db634af
Elizabeth A. Fujita <syp013@c28-0113-25.ad.mtu.edu>
2019-07-12 14:02:38 -0400

    other-stuffs

---
+++ other/java script/index.html/app.js
@@ -0,0 +1,63 @@
+
+function myfuncticon1(){
+    console.log("My first javaScript function")
+}
+
+function myfunction2(){
+    console.log("My first javaScript function2")
+}
+
+function myfunction3(){
+    console.log("My first javaScript function3")
+}
+
+function myfunction4(){
+    console.log("My first javaScript function4")
+}
+
+function add(){
+    var num1 = 5;
+    var num2 = 6;
+    var num3;
+    num3 = num1 + num2
+    alert("the sum is:"+num3)
+}
+
+function subtract(){
+    var num1 = 5;
+    var num2 = 6;
+    var num3;
+    num3 = num1 - num2
+    alert("the sum is:"+num3)
+}
+
+function multiply(){
+    var num1 = 56456354;
+    var num2 = 693865044;
+    var num3;
+    num3 = num1 * num2
+    alert("the sum is:"+num3)
+}
+
+function divide(){
+    var num1 = 5;
+    var num2 = 6;
+    var num3;
+    num3 = num1 / num2
+    alert("the sum is:"+num3)
+}
+
+function stringTest(){
+    var word1 = "how"
+    var word2 = "dy"
+    var word3;
+    word3 = word1 + word2;
+    alert(word3)
+}
+
+function pikachu(){
+    src="https://www.algemeiner.com/wp-content/uploads/2016/04/Pikachu.png"
+}
+function turnlighton(){
+    document.getElementById('bulb').src="https://gamewith-en.akamaized.net/article_tools/pokemongo/gacha/2479_i.png"
+}
---
+++ other/java script/index.html/app2.js
@@ -0,0 +1,6 @@
+function pikachu(){
+    <img src="https://www.algemeiner.com/wp-content/uploads/2016/04/Pikachu.png"
+}
+function rychu(){
+    <img src="https://gamewith-en.akamaized.net/article_tools/pokemongo/gacha/2479_i.png"
+}
